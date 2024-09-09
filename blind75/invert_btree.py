f782c8f82a288ffa1526028a189e26122231d198
Anubhav Gupta <aanubhav04@gmail.com>
2024-09-09 23:00:22 +0530

    btree

---
+++ blind75/invert_btree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+from typing import Optional
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return None
+        root.left,root.right = self.invertTree(root=root.right),self.invertTree(root=root.left)
+        return root
